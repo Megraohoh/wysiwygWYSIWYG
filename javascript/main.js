229eeadc58cba592a565c050aeb4b7625dd374bb
Meghan Debity <mdebity@gmail.com>
2017-03-20 21:03:09 -0500

    bulked up the array

--- javascript/main.js
+++ javascript/main.js
@@ -11,4 +11,92 @@
 // field, then the content of the input field should immediately 
 // be blank.
 
-var arrayWysiwyg = [];
+var arrayWysiwyg = [
+{
+  title: "Pink Lady",
+  name: "Stockard Channing",
+  bio: "Rizzo",
+  image: "http://vignette3.wikia.nocookie.net/grease/images/2/2c/Rizzo_grease.jpeg/revision/latest?cb=20121226055625"
+  lifespan: {
+    birth: 1944,
+    death: 
+  }
+}
+{
+  title: "Pink Lady",
+  name: "Didi Conn",
+  bio: "Frenchie",
+  image: "http://img.wennermedia.com/480-width/frenchie-grease-inline.jpg"
+  lifespan: {
+    birth: 1951,
+    death: 
+  }
+}
+{
+  title: "Pink Lady",
+  name: "Dinah Manoff",
+  bio: "Marty",
+  image: "http://vignette2.wikia.nocookie.net/grease/images/6/6b/Grease_297Pyxurz.jpg/revision/latest?cb=20121226055754"
+  lifespan: {
+    birth: 1956,
+    death: 
+  }
+}
+{
+  title: "Pink Lady",
+  name: "Jamie Donnelly",
+  bio: "Jan",
+  image: "http://vignette4.wikia.nocookie.net/grease/images/9/96/Lala_106296915.jpg/revision/latest?cb=20121226055500"
+  lifespan: {
+    birth: 1947,
+    death: 
+  }
+}
+{
+  title: "Almost a Pink Lady",
+  name: "Olivia Newton-John",
+  bio: "Sandy",
+  image: "http://leblow.co.uk/wp-content/uploads/2012/11/Sandy-from-Grease.jpg"
+  lifespan: {
+    birth: 1948,
+    death: 
+  }
+}
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var counter = 0;
+var outputEl = document.getElementById("outputEl");
+for (; counter < 5; counter++) {
+  // Give each person element a unique identifier
+  outputEl.innerHTML += `<div class="person__container" id="person--${counter}"></div>`;
+}
+
+// Now containerEl will have elements in it
+var containerEl = document.getElementsByClassName("person__container");
+
+// Event listeners are created
+for (var i = 0; i < containerEl.length; i++) {
+  containerEl[i].addEventListener("click", function (event) {
+    // Logic to execute when the element is clicked
+  });
+};
